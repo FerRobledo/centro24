38430b73f9ba975f302a243c2cd2f4b0786b7e3e
Fermin Robledo <fermin4robledo@gmail.com>
2025-06-05 14:53:13 -0300

    Api

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TestService } from 'src/app/services/test.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // Para usar el servicio hay que ponerlo en el constructor
+  constructor(private testServicio: TestService) { }
 
+  personas: {id: number, nombre:string}[] = []
+  cargando: boolean = true;
+
+  // AL INICIAR EL COMPONENTE LLAMA A getNombres()
   ngOnInit() {
+    this.getNombres();
   }
+
+  getNombres() {
+    // El servicio nos va a devolver un Observable con la info
+    this.testServicio.getNombres().subscribe({
+      next: (data) => {
+        // data es lo que devuelve la api
+        this.personas = data;
+      },
+      error: (error) => {
+        // Esto por si tira error
+        console.log(error)
+      },
+      complete: () => {
+        this.cargando = false;
+      }
+    })
+  }
+
 
 }
