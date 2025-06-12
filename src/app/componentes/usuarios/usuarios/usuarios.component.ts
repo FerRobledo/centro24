2c5b0970d7b6a9614cf447ae5c7f1800134b2ab2
Fermin Robledo <fermin4robledo@gmail.com>
2025-06-12 18:25:28 -0300

    Agrega logs

--- src/app/componentes/usuarios/usuarios/usuarios.component.ts
+++ src/app/componentes/usuarios/usuarios/usuarios.component.ts
@@ -24,20 +24,21 @@ export class UsuariosComponent implements OnInit {
   cargando: Boolean = false;
 
   ngOnInit() {
+    console.log("Iniciando usuarios component");
     this.cargarUsuarios();
   }
 
   cargarUsuarios() {
     this.cargando = true;
+    console.log("Cargando usuarios...")
     const userId = this.authService.getUserId();
     if (userId) {
-
       this.usuarioService.getUsuarios(userId).subscribe({
         next: data => {
           this.usuarios = data.usuarios;
         },
         error: error => { console.log(error) },
-        complete: () => { this.cargando = false},
+        complete: () => { this.cargando = false; console.log("Carga completa")},
       })
     }
   }
