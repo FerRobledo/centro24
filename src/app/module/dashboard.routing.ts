e6bfe3f516855c61293e5ce24c87962d64025718
Fermin Robledo <fermin4robledo@gmail.com>
2025-06-30 08:30:54 -0300

    Fix budle excedido

---
+++ src/app/module/dashboard.routing.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../componentes/home/home.component';
+import { DashboardComponent } from '../componentes/dashboard/dashboard.component';
+import { CobranzaComponent } from '../componentes/cobranza/cobranza.component';
+import { ProductosComponent } from '../componentes/productos/productos.component';
+import { UsuariosComponent } from '../componentes/usuarios/usuarios/usuarios.component';
+import { ClientesComponent } from '../componentes/clientes/clientes.component';
+import { authGuard } from '../auth-guard.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent,
+      },
+      {
+        path: 'cobranza',
+        component: CobranzaComponent,
+      },
+      {
+        path: 'productos',
+        component: ProductosComponent
+      },
+      {
+        path: 'usuarios',
+        component: UsuariosComponent,
+      },
+      {
+        path: 'clientes',
+        component: ClientesComponent
+      },
+    ],
+    canActivate: [authGuard]
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DashboardRoutes { }
