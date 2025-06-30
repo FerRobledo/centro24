0106a7bee7e78374c6c4eb240dc9d8c66604d223
Fermin Robledo <fermin4robledo@gmail.com>
2025-06-30 08:41:32 -0300

    fix merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,39 +5,17 @@ import { HomeComponent } from './componentes/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RegisterComponent } from './componentes/auth/register/register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DashboardComponent } from './componentes/dashboard/dashboard.component';
 import { LoginComponent } from './componentes/auth/login/login.component';
 import { SideMenuComponent } from './componentes/sideMenu/sideMenu.component';
-import { CardComponent } from './componentes/card/card.component';
-import { InformacionComponent } from './componentes/informacion/informacion.component';
-import { EstadisticasComponent } from './componentes/estadisticas/estadisticas.component';
-import { ProductosComponent } from './componentes/productos/productos.component';
-import { UsuariosComponent } from './componentes/usuarios/usuarios/usuarios.component';
 import { MatDialogModule } from '@angular/material/dialog';
-import { UsuarioModalComponent } from './componentes/usuarios/usuarioModal/UsuarioModal.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatInputModule } from '@angular/material/input';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
-import { UsuarioCardComponent } from './componentes/usuarios/usuarioCard/usuarioCard.component';
-import { CobranzaComponent } from './componentes/cobranza/cobranza.component';
-import { RegisterClienteComponent } from './componentes/cobranza/registerCliente/registerCliente.component';
-import { ConfirmDialogComponent } from './componentes/confirmDialog/confirmDialog.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { ClientesComponent } from './componentes/clientes/clientes.component';
-import { InsertarClienteComponent } from './componentes/clientes/insertarCliente/insertarCliente.component';
-import { CargaProductosComponent } from './componentes/cargaProductos/cargaProductos.component';
-import { AgregarPagoModalComponent } from './componentes/clientes/agregarPagoModal/agregarPagoModal.component';
-import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
-import { MatSelectModule } from '@angular/material/select';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatMomentDateModule, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
 import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatTabsModule } from '@angular/material/tabs';
-import { ListaClientesComponent } from './componentes/clientes/listaClientes/listaClientes.component';
-import { ListaPagosComponent } from './componentes/clientes/listaPagos/listaPagos.component';
 import { LOCALE_ID } from '@angular/core';
 
 
@@ -61,11 +39,6 @@ export const MONTH_YEAR_FORMATS = {
     SideMenuComponent,
     RegisterComponent,
     LoginComponent,
-
-    // a dashboardmodule
-    AgregarPagoModalComponent,
-    ListaClientesComponent,
-    ListaPagosComponent,
   ],
   imports: [
     BrowserModule,
@@ -81,14 +54,6 @@ export const MONTH_YEAR_FORMATS = {
     MatInputModule,
     MatIconModule,
     MatTooltipModule,
-    // a dashboardModule
-    MatOptionModule,
-    MatSelectModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatMomentDateModule,
-    MatDividerModule,
-    MatTabsModule,
   ],
   providers: [
     provideAnimationsAsync(),
