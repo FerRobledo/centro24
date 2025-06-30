5ab63003d6f557d45a9396a883f7a61f48dcb9ef
Matias Fleitas <matifleitas6@gmail.com>
2025-06-30 15:45:13 -0300

    Estadisticas dinamicas con valores reales, falta cantidad de users y recaudacion del dia de ayer

---
+++ api/estadisticas/[id].js
@@ -0,0 +1,104 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+});
+
+module.exports = async (req, res) => {
+    const origin = req.headers.origin || '*';
+
+    //seteo los encabzados para http
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Vary', 'Origin');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
+    //GET
+    if (req.method === 'GET') {
+        //leer el id desde la query
+        const { id, action } = req.query;
+
+        if (!id) {
+            return res.status(400).json({ error: 'Error falta id para obtener las stats', details: error.message });
+        }
+        try {
+            if (action === 'previous') {
+                const result = await pool.query(    
+                    "SELECT COALESCE((" + 
+                    "SELECT SUM(pm.monto) " +
+                    "FROM public.pagos_mensuales pm " +
+                    "JOIN public.clientes_mensuales cm on (pm.id_client = cm.id_client) " +
+                    "WHERE pm.fecha_pago >= date_trunc('month', CURRENT_DATE - INTERVAL '1 month') " +
+                    "AND pm.fecha_pago < date_trunc('month', CURRENT_DATE) " +
+                    "AND cm.user_admin = $1),0)" +
+                    "+" +
+                    "COALESCE(("+
+                        "SELECT SUM(efectivo + debito + transferencia + cheque + gasto + retiro)" +
+                        "FROM caja" +
+                        " WHERE fecha >= date_trunc('month', CURRENT_DATE - INTERVAL '1 month') " +
+                        " AND fecha < date_trunc('month', CURRENT_DATE) " +
+                        " AND user_admin = $1),0) AS total",[id]);
+
+                const total = result.rows[0].total ?? 0;
+                //date_trunc('month', CURRENT_DATE)
+                // Da el primer día del mes actual a las 00:00 hs.
+                //date_trunc('month', CURRENT_DATE + INTERVAL '1 month')
+                // Da el primer día del mes siguiente a las 00:00 hs.
+                return res.status(200).json( total );
+            }
+            else if (action === 'current') {
+                const result = await pool.query(`
+                        SELECT
+                        COALESCE((
+                            SELECT SUM(pm.monto)
+                            FROM public.pagos_mensuales pm
+                            JOIN public.clientes_mensuales cm ON pm.id_client = cm.id_client
+                            WHERE pm.fecha_pago >= date_trunc('month', CURRENT_DATE)
+                            AND pm.fecha_pago < date_trunc('month', CURRENT_DATE + INTERVAL '1 month')
+                            AND cm.user_admin = $1), 0)
+                        +
+                        COALESCE((
+                            SELECT SUM(efectivo + debito + transferencia + cheque + gasto + retiro)
+                            FROM caja
+                            WHERE fecha >= date_trunc('month', CURRENT_DATE)
+                            AND fecha < date_trunc('month', CURRENT_DATE + INTERVAL '1 month')
+                            AND user_admin = $1
+                        ), 0) AS total;`,[id]);
+                const total = result.rows[0].total ?? 0;
+                return res.status(200).json( total );
+            }
+            else if (action === 'clients') {
+                const { rows } = await pool.query(`
+                        SELECT COUNT(*) FROM public.clientes_mensuales WHERE user_admin=$1;`,[id]);
+                const total = parseInt(rows[0].count) || 0; // → result.rows[0] ?? 0; esta mal pq el count esta todavia{ count: '5' }
+                return res.status(200).json( total );
+            }
+            else if (action === 'newClients') {
+                const { rows } = await pool.query(`
+                        SELECT COUNT(*) AS cant
+                            FROM public.clientes_mensuales cm
+                            JOIN public.pagos_mensuales pm ON (cm.id_client = pm.id_client)
+                            WHERE cm.user_admin=$1
+                            AND pm.fecha_pago >= date_trunc('month', CURRENT_DATE) 
+                            AND pm.fecha_pago < date_trunc('month', CURRENT_DATE + INTERVAL '1 month')`,[id]);
+                const total = parseInt(rows[0].cant) || 0; 
+                return res.status(200).json( total );
+            }
+            else {
+                console.log("no entre a ninguno");
+            }
+
+            return res.status(200).json(rows);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: 'Error no se pudo obtener los clientes del dia', details: error.message });
+        }
+    }
+
+
+}
--- src/app/componentes/estadisticas/estadisticas.component.ts
+++ src/app/componentes/estadisticas/estadisticas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { EstadisticasService } from 'src/app/services/estadisticas.service';
+
 
 @Component({
   selector: 'app-estadisticas',
@@ -6,10 +9,93 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./estadisticas.component.css']
 })
 export class EstadisticasComponent implements OnInit {
+  collectionPreviousMonth:number = 0;
+  collectionCurrentMonth:number = 0;
+  cantClients:number = 0;
+  newClients:number = 0;
+  //demas stats de las cards
 
-  constructor() { }
+  constructor(private estadisticasService: EstadisticasService, private authService: AuthService) { }
 
   ngOnInit() {
+    this.loadPreviousMonth();
+    this.loadCurrentMonth();
+    this.loadClients();
+    this.loadNewClients();
   }
 
+  loadPreviousMonth() {
+    const id = this.authService.getIdAdmin();
+
+    if (id) {
+      this.estadisticasService.getStatsPreviousMonth(id).subscribe({
+        next: (data) => {
+          this.collectionPreviousMonth = data;
+          console.log("El total recaudado del mes anterior es de: ", data);
+        },
+        error: (error) => {
+          console.log("Error en el pedido de clientes del dia: ", error);
+          
+        },
+        complete: () => {
+          console.log("Pedido en estado OK");
+        }
+      })
+    }
+  }
+
+  loadCurrentMonth() {
+    const id = this.authService.getIdAdmin();
+    if(id) {
+      this.estadisticasService.getStatsCurrenMonth(id).subscribe({
+        next: (data) => {
+          this.collectionCurrentMonth = data;
+          console.log("El total recaudado del mes actual es de: ", data);
+        },
+        error: (error) => {
+          console.log("Error en el pedido de clientes del dia: ", error);
+          
+        },
+        complete: () => {
+          console.log("Pedido en estado OK");
+        }
+      })
+    }
+  }
+
+  loadClients() {
+    const id = this.authService.getIdAdmin();
+    if(id) {
+      this.estadisticasService.getClients(id).subscribe({
+        next: (data) => {
+          this.cantClients = data;
+          console.log("El total de clientes es: ", data);
+        },
+        error: (error) => {
+          console.log("Error en el pedido de clientes del dia: ", error);
+        },
+        complete: () => {
+          console.log("Pedido en estado OK");
+        }
+      })
+    }
+  }
+
+  loadNewClients(){
+    const id = this.authService.getIdAdmin();
+    if(id) {
+      this.estadisticasService.getNewClients(id).subscribe({
+        next: (data) => {
+          this.newClients = data;
+          console.log("El total de nuevos clientes es: ", data);
+        },
+        error: (error) => {
+          console.log("Error en el pedido de clientes del dia: ", error);
+        },
+        complete: () => {
+          console.log("Pedido en estado OK");
+        }
+      })
+    }
+  }
 }
