2e4230f9b38225c15574849921db41939924c3d6
Fermin Robledo <fermin4robledo@gmail.com>
2025-07-21 10:26:25 -0300

    Optimizacion Bundle inicial

--- src/app/module/dashboard.routing.ts
+++ src/app/module/dashboard.routing.ts
@@ -6,8 +6,6 @@ import { CobranzaComponent } from '../componentes/cobranza/cobranza.component';
 import { ProductosComponent } from '../componentes/productos/productos.component';
 import { UsuariosComponent } from '../componentes/usuarios/usuarios/usuarios.component';
 import { ClientesComponent } from '../componentes/clientes/clientes.component';
-import { authGuard } from '../auth-guard.guard';
-import { NotFoundComponent } from '../componentes/notFound/notFound.component';
 
 const routes: Routes = [
   {
