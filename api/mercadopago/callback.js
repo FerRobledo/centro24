b8cdc60893958f7ec739be6c832909bd63f3a20b
Fermin Robledo <fermin4robledo@gmail.com>
2025-12-04 00:58:14 -0300

    Elimina referencia en body

--- api/mercadopago/callback.js
+++ api/mercadopago/callback.js
@@ -17,28 +17,24 @@ module.exports = async (req, res) => {
 
     console.log("Webhook recibido:", JSON.stringify(req.body, null, 2));
     // MercadoPago envía el ID del pago en `data.id`
-    const { type, data } = req.body;
 
-    if (type === "payment") {
-        const paymentId = data.id;
 
-        // Consultar a MercadoPago para obtener detalles del pago
-        const { data: payment } = await axios.get(
-            `https://api.mercadopago.com/v1/payments/${paymentId}`,
-            {
-                headers: { Authorization: `Bearer ${await getAccessTokenValido(usuario)}` }
-            }
-        );
+    // // Consultar a MercadoPago para obtener detalles del pago
+    // const { data: payment } = await axios.get(
+    //     `https://api.mercadopago.com/v1/payments/135763152487`,
+    //     {
+    //         headers: { Authorization: `Bearer ${await getAccessTokenValido(usuario)}` }
+    //     }
+    // );
 
-        console.log("Detalle del pago:", payment);
-        const estadoPago = payment.status; // approved, rejected, pending
-        const referencia = payment.external_reference; // tu id_venta
-        console.log(`Pago ${payment.id} | Estado: ${estadoPago} | Ref: ${referencia}`);
-        if (referencia) {
-            console.log(referencia);
+    // console.log("Detalle del pago:", payment);
+    // const estadoPago = payment.status; // approved, rejected, pending
+    // const referencia = payment.external_reference; // tu id_venta
+    // console.log(`Pago ${payment.id} | Estado: ${estadoPago} | Ref: ${referencia}`);
+    // if (referencia) {
+    //     console.log(referencia);
 
-        }
-    }
+    // }
     // Responder 200 a MercadoPago (OBLIGATORIO)
     res.status(200).end();
 };
