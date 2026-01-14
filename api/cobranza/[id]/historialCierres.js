f9c550e48d3dff4edfc26d404d9dd4fd8f61800a
Fermin Robledo <fermin4robledo@gmail.com>
2026-01-14 02:19:37 -0300

    Migracion api

---
+++ api/cobranza/[id]/historialCierres.js
@@ -0,0 +1,47 @@
+import pool from '../../db.js';
+import { requireAuth } from '../../protected/requireAuth.js';
+
+export default async function handler(req, res) {
+    const origin = req.headers.origin || '*';
+
+    //seteo los encabzados para http
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Vary', 'Origin');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    //le pregunto al back si puedo usar PUT,POST,... con el OPTIONS
+    //retorno si, los podes usar
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
+    // Autenticación
+    try {
+        req.user = requireAuth(req);
+    } catch (e) {
+        return res.status(401).json({ error: 'No autorizado', details: e.message });
+    }
+
+    if (req.method === 'GET') {
+        const { id } = req.query;
+        try {
+            const result = await pool.query(`
+                        SELECT id, fecha, monto, nombre_usuario
+                        FROM public.historial_cierres
+                        WHERE user_admin = $1
+                        ORDER BY fecha DESC;
+                        `, [id]);
+
+            return res.status(200).json(result.rows);
+
+        } catch (error) {
+
+            console.error('error al obtener historial de caja:', error);
+            return res.status(500).json({ error: 'Error no se pudo obtener el historial de caja', details: error.message });
+
+        }
+    }
+
+    res.status(405).json({ message: 'Método no permitido' });
+}
--- src/app/services/cobranza.service.ts
+++ src/app/services/cobranza.service.ts
@@ -7,44 +7,44 @@ import { Observable } from 'rxjs';
 })
 export class CobranzaService {
   private origin = window.location.origin;
-  
+
   constructor(private http: HttpClient) { }
-  
+
   public getClientsOfDay(id: number): Observable<any> {
     return this.http.get(this.origin + '/api/cobranza/' + id)
   }
-  
+
   public closeClientsOfDay(id: number, nameUser: string): Observable<any> {
-    return this.http.get(this.origin + '/api/cobranza/' + id + '?action=close&nameUser=' + nameUser);
+    return this.http.post( `${this.origin}/api/cobranza/${id}/cerrarCaja/?nameUser=${nameUser}`, {});
   }
 
-  public getClientsByDate(idAdmin: number, date: string): Observable<any>{
-    return this.http.get(this.origin + '/api/cobranza/' + idAdmin + '?action=getClientsByDate&date=' +  date )
+  public getClientsByDate(idAdmin: number, date: string): Observable<any> {
+    return this.http.get(`${this.origin}/api/cobranza/${idAdmin}/ventasPorDia?date=${date}`)
   }
 
-  public getHistorialByDate(idAdmin: number, date: string): Observable<any>{
-    return this.http.get(this.origin + '/api/cobranza/' + idAdmin + '?action=getHistorialByDate&date=' +  date )
+  public getHistorialByDate(idAdmin: number, date: string): Observable<any> {
+    return this.http.get(`${this.origin}/api/cobranza/${idAdmin}/historialCierrePorDia?date=${date}`)
   }
 
   public postClientDaily(payload: any, idAdmin: number): Observable<any> {
     return this.http.post(this.origin + '/api/cobranza/' + idAdmin, payload);
   }
 
-  public updateClient(idClient: number, idAdmin: number, payload: any) {  
+  public updateClient(idClient: number, idAdmin: number, payload: any) {
     const body = { ...payload, idAdmin };
     return this.http.put(this.origin + '/api/cobranza/' + idClient, body);
   }
 
-  public deleteClient(idAdmin: number, idClient: number){
+  public deleteClient(idAdmin: number, idClient: number) {
     const body = { idClient };
     return this.http.delete(this.origin + '/api/cobranza/' + idAdmin, { body });
   }
 
   public getHistory(id: number): Observable<any> {
-    return this.http.get(this.origin + '/api/cobranza/' + id + '?action=history')
+    return this.http.get( `${this.origin}/api/cobranza/${id}/historialCierres`)
   }
-  
+
   public getDetailsId(id: number, idCierre: number): Observable<any> {
-    return this.http.get(this.origin + '/api/cobranza/' + id + '?action=details&idCierre=' + idCierre)
+    return this.http.get( `${this.origin}/api/cobranza/${id}/detallesCierre/?idCierre= ` + idCierre)
   }
 }
